0d7aeccd7126001199d391445fe9512d1969df43
Romel <200869522+rblamug@users.noreply.github.com>
2025-05-23 11:37:47 -0400

    Completed task

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let lowerCaseString = string.toLowerCase().replace(/[^\w]/g, "");
+    return lowerCaseString === lowerCaseString.split("").reverse().join("");
 };
 
 // Do not edit below this line
