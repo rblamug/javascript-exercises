8df2921618af487f1f0ca60e78d80f8286832656
Romel <200869522+rblamug@users.noreply.github.com>
2025-05-27 18:48:42 -0400

    Wrote inital function using array methods

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,9 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(arr) {
+    return arr.map((person) => ({
+        name: person.name,
+        age: person.yearOfDeath - person.yearOfBirth
+    })).reduce((max, current) => current.age > max.age ? current : max);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
