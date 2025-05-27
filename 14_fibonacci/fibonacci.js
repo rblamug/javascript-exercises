d9183167c91d749525450f42b127e1a250cc34ea
Romel <200869522+rblamug@users.noreply.github.com>
2025-05-27 13:01:26 -0400

    Solved using recursion

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = Number(n);
+    if (n < 0) return "OOPS";
+    if (n <= 1) return n;
+    return fibonacci(n - 2) + fibonacci(n - 1);
 };
 
 // Do not edit below this line
