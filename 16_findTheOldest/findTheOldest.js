a5b6b063e7bcd0eb17ea3a0d6a260c31def733a2
Romel <200869522+rblamug@users.noreply.github.com>
2025-05-27 18:59:46 -0400

    add ability to get current year if no death date
    all tests should run successfully

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,7 +1,7 @@
 const findTheOldest = function(arr) {
     return arr.map((person) => ({
         name: person.name,
-        age: person.yearOfDeath - person.yearOfBirth
+        age: person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : new Date().getFullYear() - person.yearOfBirth
     })).reduce((max, current) => current.age > max.age ? current : max);
 }
 
